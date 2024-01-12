3d63c991c05b540624cdf7451f4ab8a059ba45c9
agmcoding <antonioibmachado@gmail.com>
2024-01-12 11:22:09 -0300

    Add simple users array
    Add temporary simple users array for new HTTP requests

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const usersArray = [
+    { "ID": "bobid", "username": "Bob", "email": "bob@example.com" },
+    { "ID": "amyid", "username": "Amy", "email": "amy@example.com" },
+    { "ID": "dukeid", "username": "Duke", "email": "duke@example.com" }
+];
+
 app.get('/welcome/', (request, response) => {
     response.status(200).send('Welcome!');
+});
+
+app.get('/api/users/', (request, response) => {
+    response.status(200).send(usersArray);
 });
 
 app.listen(port, () => {

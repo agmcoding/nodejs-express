d8a367998af25497ff23fae2d147e8692112abc4
agmcoding <antonioibmachado@gmail.com>
2024-01-12 12:59:30 -0300

    Feat: Add more HTTP request handlers
    Add HTTP PUT and DELETE request handlers

--- index.js
+++ index.js
@@ -17,6 +17,52 @@ app.get('/api/users/', (request, response) => {
     response.status(200).send(Array.from(usersMap));
 });
 
+app.get('/api/users/:ID', (request, response) => {
+    const ID = request.params.ID;
+
+    if (usersMap.has(ID) === false) {
+        response.status(404).send(`User not found with ID: ${ID}`);
+        return;
+    }
+
+    const user = usersMap.get(ID);
+    response.status(200).send(user);
+});
+
+app.put('/api/users/:ID', (request, response) => {
+    const ID = request.params.ID;
+    const { username, email } = request.body;
+
+     if (username === undefined) {
+        response.status(400).send('Missing username');
+        return;
+    } else if (email === undefined) {
+        response.status(400).send('Missing email');
+        return;
+    }
+
+    if (usersMap.has(ID) === false) {
+        response.status(404).send(`User not found with ID: ${ID}`);
+        return;
+    }
+
+    usersMap.set(ID, request.body);
+    response.status(204).end();
+});
+
+app.delete('/api/users/:ID', (request, response) => {
+    const ID = request.params.ID;
+
+    const userDeleted = usersMap.delete(ID);
+
+    if (userDeleted === false) {
+        response.status(404).send(`User not found with ID: ${ID}`);
+        return;
+    }
+
+    response.status(204).end(); 
+});
+
 app.listen(port, () => {
     console.log('Server starting...');
     console.log(`Listening on port ${port}`);

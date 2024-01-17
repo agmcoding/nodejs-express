eb59998c507cd48031cfdad27e79e1d37b145e39
agmcoding <antonioibmachado@gmail.com>
2024-01-17 15:19:34 -0300

    Build: Add airbnb base eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,26 @@
 module.exports = {
-  "env": {
-    "node": true
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": "latest",
-    "sourceType": "module"
-  }
-}
+  extends: 'airbnb-base',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};

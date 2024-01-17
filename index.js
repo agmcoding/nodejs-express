1c438ed16793eacb5661794f05247a40c1f1a46e
agmcoding <antonioibmachado@gmail.com>
2024-01-17 15:53:17 -0300

    Refactor: Solve erros pointed by eslinter

--- index.js
+++ index.js
@@ -1,90 +1,94 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
 const { randomUUID } = require('crypto');
 
+const app = express();
 app.use(express.json());
+const port = process.env.PORT || 3000;
 
 const usersMap = new Map();
-usersMap.set(randomUUID(), { "username": "Bob", "email": "bob@example.com" });
-usersMap.set(randomUUID(), { "username": "Amy", "email": "amy@example.com" });
-usersMap.set(randomUUID(), { "username": "Duke", "email": "duke@example.com" });
+usersMap.set(randomUUID(), { username: 'Bob', email: 'bob@example.com' });
+usersMap.set(randomUUID(), { username: 'Amy', email: 'amy@example.com' });
+usersMap.set(randomUUID(), { username: 'Duke', email: 'duke@example.com' });
 
 app.get('/welcome/', (request, response) => {
-    response.status(200).send('Welcome!');
+  response.status(200).send('Welcome!');
 });
 
 app.get('/api/users/', (request, response) => {
-    response.status(200).send(Array.from(usersMap));
+  response.status(200).send(Array.from(usersMap));
 });
 
 app.get('/api/users/:ID', (request, response) => {
-    const ID = request.params.ID;
+  const { ID } = request.params;
 
-    if (usersMap.has(ID) === false) {
-        response.status(404).send(`User not found with ID: ${ID}`);
-        return;
-    }
+  if (usersMap.has(ID) === false) {
+    response.status(404).send(`User not found with ID: ${ID}`);
+    return;
+  }
 
-    const user = usersMap.get(ID);
-    response.status(200).send(user);
+  const user = usersMap.get(ID);
+  response.status(200).send(user);
 });
 
 app.post('/api/users/', (request, response) => {
-    const { username, email } = request.body;
+  const { username, email } = request.body;
 
-     if (username === undefined) {
-        response.status(400).send('Missing username');
-        return;
-    } else if (email === undefined) {
-        response.status(400).send('Missing email');
-        return;
-    }
+  if (username === undefined) {
+    response.status(400).send('Missing username');
+    return;
+  }
 
-    const ID = randomUUID();
-    usersMap.set(ID, { "username": username, "email": email });
+  if (email === undefined) {
+    response.status(400).send('Missing email');
+    return;
+  }
 
-    const createdUser = usersMap.get(ID);
-    const result = [ ID, createdUser ];
+  const ID = randomUUID();
+  usersMap.set(ID, { username, email });
 
-    response.status(201).send(result);
+  const createdUser = usersMap.get(ID);
+  const result = [ID, createdUser];
+
+  response.status(201).send(result);
 });
 
 app.put('/api/users/:ID', (request, response) => {
-    const ID = request.params.ID;
-    const { username, email } = request.body;
+  const { ID } = request.params;
+  const { username, email } = request.body;
 
-     if (username === undefined) {
-        response.status(400).send('Missing username');
-        return;
-    } else if (email === undefined) {
-        response.status(400).send('Missing email');
-        return;
-    }
+  if (username === undefined) {
+    response.status(400).send('Missing username');
+    return;
+  }
 
-    if (usersMap.has(ID) === false) {
-        response.status(404).send(`User not found with ID: ${ID}`);
-        return;
-    }
+  if (email === undefined) {
+    response.status(400).send('Missing email');
+    return;
+  }
 
-    usersMap.set(ID, request.body);
-    response.status(204).end();
+  if (usersMap.has(ID) === false) {
+    response.status(404).send(`User not found with ID: ${ID}`);
+    return;
+  }
+
+  usersMap.set(ID, request.body);
+  response.status(204).end();
 });
 
 app.delete('/api/users/:ID', (request, response) => {
-    const ID = request.params.ID;
+  const { ID } = request.params;
 
-    const userDeleted = usersMap.delete(ID);
+  const userDeleted = usersMap.delete(ID);
 
-    if (userDeleted === false) {
-        response.status(404).send(`User not found with ID: ${ID}`);
-        return;
-    }
+  if (userDeleted === false) {
+    response.status(404).send(`User not found with ID: ${ID}`);
+    return;
+  }
 
-    response.status(204).end(); 
+  response.status(204).end();
 });
 
 app.listen(port, () => {
-    console.log('Server starting...');
-    console.log(`Listening on port ${port}`);
+  console.log('Server starting...');
+  console.log(`Listening on port ${port}`);
 });
